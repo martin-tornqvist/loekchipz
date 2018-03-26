3ebdff51877d723b8affb476acfd2733434801e5
Martin Törnqvist <m.tornq@gmail.com>
2018-03-26 22:13:09 +0200

    Removed whitespace-mode toggling in .dir-locals.el
    * .dir-locals.el now only sets project variables (as it should be)
    * whitespace-mode should be enabled and configured in the user local
      emacs config (what to highlight, etc), for those who want to use it
      (for example in a c++-mode-hook).

--- .dir-locals.el
+++ .dir-locals.el
@@ -8,16 +8,7 @@
  (c++-mode
   . (
      (fill-column . 80)
-     ;; NOTE: This will not have an effect until whitespace-mode is (re)started.
      (whitespace-line-column . 80)
      (c-file-style . "bsd")
-     ;; Disable/enable whitespace-mode, see note above.
-     ;; NOTE: This is "unsafe", see:
-     ;; M-x describe-variable -> enable-local-variables
-     ;; This will not be evaluated if enable-local-variables is set to ":safe".
-     (eval ignore-errors
-           (require 'whitespace)
-           (whitespace-mode 0)
-           (whitespace-mode 1)))
-  )
- )
+     )
+  ))
